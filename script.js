c22ffa113aa4e17d7874c3fd2305799e722acf74
DavidNava0387 <fnavas2200@alumno.ipn.mx>
2024-09-24 18:12:43 -0600

    Tal vez - Paulo Londra

---
+++ script.js
@@ -0,0 +1,28 @@
+//CLASE 24/09/2024
+//NOMBRE : NAVA SOSA FRANCISCO DAVID
+
+//VARIABLES
+
+/* 
+LAS VARUABLES GUARDAN INFORMACION QUE PUEDES REUTILIZAR
+*/
+
+//Dcelaracion de variables
+
+let nombre = 'Pedro'; //usa let para declarar variables que pueden cambiar su valor
+const PI = 3.1416;
+var edad = 25;
+
+//operadores
+let a = 5;
+let e = 2;
+let suma = a + e; //7
+let resta = a - e; //3
+let multiplicacion = a * e; //10
+let division = a / e; //2.5
+
+//operadores de comparacion
+a > e; //true
+a < e; //false
+a == e; //false
+a === e; //false ( compara valor y tipo)
