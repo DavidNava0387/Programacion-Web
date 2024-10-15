437620e9f4a9334cf973c1fa5581fd3873309cf3
DavidNava0387 <fnavas2200@alumno.ipn>
2024-10-15 17:55:34 -0600

    15/10/2024

---
+++ Clase_15102024/script.js
@@ -0,0 +1,51 @@
+//Definicion de una funcion 
+/*
+function hello(){
+    console.log('Hola clase');
+    console.log(' 5IV7');
+}
+
+//Llamada a la funcion
+hello();
+hello();
+hello();
+hello();
+hello();
+*/
+/*
+//Retorno de una funcion
+function hello(){
+    return 'Hola clase';
+}
+
+const result = hello();
+
+console.log(result);
+*/
+/*
+//Funcion que retorna otra funcion
+function hello(){
+    return function (){
+        return 'hola soy la funcion 2';
+    }
+}
+
+console.log(hello()())
+
+*/
+/*
+function add(x){
+
+    console.log(x);
+
+}
+
+add(5)
+*/
+
+function add(x,y){
+
+    console.log(x+y);
+
+}
+add(5,10)
