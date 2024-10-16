cb780e7b2c04196e93c75cda21e1aead8571ff94
DavidNava0387 <fnavas2200@alumno.ipn>
2024-10-15 18:39:57 -0600

    15/10/2024

--- Clase_15102024/script.js
+++ Clase_15102024/script.js
@@ -42,10 +42,72 @@ function add(x){
 
 add(5)
 */
-
+/*
+//Multiparametros
 function add(x,y){
 
     console.log(x+y);
-
 }
-add(5,10)
+add(5,undefined)
+*/
+
+//const { use } = require("chai");
+
+
+//Control de error en multiparametros
+/*
+function add(x,y){
+
+    if (y===undefined) {
+        y=0;
+    }
+
+    console.log(x+y);
+}
+add(5,undefined)
+*/
+
+//parametro de tipo cadena
+/*function add(name){
+
+
+    console.log('Hola '+name);
+}
+
+add("Clase JS");
+
+*/
+
+//Objetos
+const user = {
+    nombre: 'David',
+    apellidoP: 'Nava',
+    apellidoM: 'Sosa',
+    edad: 17,
+    direccion :{
+        calle: 'Mar Mediterraneo',
+        no: 2018,
+        colonia: 'Popotla',
+        delegacion: 'Miguel Hidalgo'
+    },
+    amigos: ['Emiliano', 'Karen'],
+    activo: true
+}
+
+//consola nombre, apellidoP, apellidoM
+//alert activo
+//console edad
+//alert calle, no, colonia, delegacion
+//consola amigos
+
+function usuario(){
+    console.log(user.nombre + " "+ user.apellidoP + " " + user.apellidoM);
+    if (user.activo === true) {
+      alert("Activo");  
+    }
+    console.log(user.edad);
+    alert(user.direccion.calle + " " + user.direccion.no + " " + user.direccion.colonia + " " + user.direccion.delegacion);
+    console.log(user.amigos[0] + " " + user.amigos[1]);
+}
+
+usuario();
